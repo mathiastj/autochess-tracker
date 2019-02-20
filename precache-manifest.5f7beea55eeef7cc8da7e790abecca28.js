6422dd83d4b1a37b189d4a1cb6d6430f47d44355
mathiastj <2278040+mathiastj@users.noreply.github.com>
2019-02-20 20:09:08 +0100

    Updates

---
+++ precache-manifest.5f7beea55eeef7cc8da7e790abecca28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7c0162ecdb376dd6146",
+    "url": "/autochess-tracker/static/css/main.5d3c1d90.chunk.css"
+  },
+  {
+    "revision": "d7c0162ecdb376dd6146",
+    "url": "/autochess-tracker/static/js/main.d7c0162e.chunk.js"
+  },
+  {
+    "revision": "e8ee5bcb2f7be7f1515f",
+    "url": "/autochess-tracker/static/js/runtime~main.e8ee5bcb.js"
+  },
+  {
+    "revision": "a196d07a7b3ce66bc2af",
+    "url": "/autochess-tracker/static/js/2.a196d07a.chunk.js"
+  },
+  {
+    "revision": "3d5793adc467a2113a25e77d0bb1a580",
+    "url": "/autochess-tracker/index.html"
+  }
+];
